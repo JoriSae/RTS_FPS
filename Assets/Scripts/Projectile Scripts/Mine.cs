5a0f9b1b6870e05b64b186586e9edcf2ce9370bf
Jori <Jozza710@hotmail.com>
2016-11-25 02:35:49 +1000

    Added 3D models for turret and mine, added script to make the mine move and blink, fixed hard coded values and commented everything.

--- Assets/Scripts/Projectile Scripts/Mine.cs
+++ Assets/Scripts/Projectile Scripts/Mine.cs
@@ -1,15 +1,133 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Mine : ProjectileController
 {
+    #region Variables
+    //External Objects
+    public GameObject lightGameObject;
+
+    //Colour Variables
+    public List<Color> mineColourList;
+
+    private MeshRenderer meshRenderer;
+
+    private Light mineLight;
+
+    //Timer Variables
+    [SerializeField] private float lightBlinkRate;
+
+    private float spawnTime;
+    private float lifeTime;
+    private float moveTimer;
+
+    //Armed Variables
+    private bool isArmed = false;
+
+    //Position Variables
+    private Vector3 latePosition;
+    private Vector3 startPosition;
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private Vector3 destination;
+    private Vector3 targetLocation;
+
+    private float distance;
+
+    //Size Variables
+    [SerializeField] private float sizeOffset;
+    #endregion
+
     protected override void Start()
     {
+
         base.Start();
+
+        //Get Components
+        meshRenderer = lightGameObject.GetComponent<MeshRenderer>();
+        mineLight = GetComponent<Light>();
+
+        //Setting spawn time
+        spawnTime = Time.time;
+
+        //Get mouse position in world units
+        mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.transform.position.y;
+        mousePosition = (Camera.main.ScreenToWorldPoint(mousePosition));
+        mousePosition.y = transform.position.y;
+
+        //Set start position
+        startPosition = Vector3.zero;
+
+        //Calculate distance
+        float locationDistance = Vector3.Distance(transform.position, mousePosition);
+        float turretDistance = locationDistance - Vector3.Distance(transform.position, startPosition) + sizeOffset;
+
+        //Calculate target location
+        targetLocation = (transform.position * 2) + (transform.forward * turretDistance);
+
+        //Reset rotation
+        transform.rotation = Quaternion.identity;
     }
 
     protected override void Update()
     {
         base.Update();
+
+        UpdateTimers();
+        UpdateMovement();
+
+        //Check if the mine is armed
+        if (isArmed)
+        {
+            UpdateLight();
+        }
+
+        isArmed = latePosition == transform.position ? true : false;
+    }
+
+    void UpdateMovement()
+    {
+        //Get direction
+        direction = targetLocation - transform.position;
+        distance = direction.magnitude;
+        direction = direction.normalized;
+
+        //Set movespeed
+        float move = bulletSpeed * Time.deltaTime;
+
+        //Set move conditions
+        if (move > distance) { move = distance; }
+
+        //Move in set direction at given speed
+        transform.Translate(direction * move);
+    }
+
+    void UpdateLight()
+    {
+        //Bounce number between 0 and 1
+        float blinkTimer = Mathf.PingPong(lifeTime, lightBlinkRate) / lightBlinkRate;
+        
+        //Lerp between colours
+        meshRenderer.material.color = Color.Lerp(mineColourList[0], mineColourList[1], blinkTimer);
+
+        //Change light intensity
+        mineLight.intensity = blinkTimer;
+    }
+
+    void UpdateTimers()
+    {
+        //Updating life time
+        lifeTime = Time.time - spawnTime;
+
+        //Update move timer
+        moveTimer = Mathf.Clamp01(moveTimer += Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        //Store late position
+        latePosition = transform.position;
     }
 }
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,240 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class TurretController : MonoBehaviour
+{
+    #region Variables
+    //States
+    public enum WeaponState
+    {
+        firstEnum = -1,
+        normalProjectile,
+        laserBeam,
+        lastEnum
+    }
+
+    public WeaponState weaponState;
+
+    //Editor Toggle Variables
+    bool toggleColours = false;
+
+    //External Objects
+    public List<Color> colourList;
+    
+    private MeshRenderer turretMaterial;
+    private MeshRenderer muzzelMaterial;
+
+    public List<ProjectileController> projectiles;
+    public ProjectileController currentProjectile;
+
+    public Transform barrel;
+    public Transform firePoint;
+
+    public GameObject muzzel;
+    public GameObject top;
+
+    public Vector3 barrelOrigin;
+
+    //Firing Variables
+    private bool firing;
+
+    private float fireRate;
+    private float timeOfFire;
+
+    //Speed Variables
+    public float rotateSpeed;
+
+    //Back Fire Variables
+    public AnimationCurve inBarrelRecoilCurve;
+    public AnimationCurve outBarrelRecoilCurve;
+
+    Vector3 barrelStartPosition;
+
+    public float minBackFire;
+    public float maxBackFire;
+
+    public float fireAnimationTime;
+    private float reloadAnimationTime;
+
+    private float backFireTime;
+
+    //Muzzel Variables
+    private Vector3 initialMuzzelSize;
+    public float muzzelExpansionSize;
+
+    //Weapon Swap Variables
+    public float weaponSwapDelay;
+    private float weaponSwapTimer;
+
+    private int scrollInput;
+    #endregion
+
+    void Start()
+    {
+        //Get component
+        muzzelMaterial = muzzel.GetComponent<MeshRenderer>();
+        turretMaterial = top.GetComponent<MeshRenderer>();
+
+        //Set start position
+        barrelStartPosition = gameObject.transform.localPosition + barrelOrigin;
+
+        initialMuzzelSize = muzzel.GetComponent<Transform>().localScale;
+    }
+
+    void Update()
+    {
+        UpdateInput();
+        UpdateState();
+        RotateTurret();
+        UpdateTimers();
+        UpdateWeapon();
+    }
+
+    void UpdateTimers()
+    {
+        //Reduce weapon swap timer
+        weaponSwapTimer -= Time.deltaTime;
+    }
+
+    void UpdateInput()
+    {
+        //Check if the player can fire and if the assigned fire key is pressed, if so fire projectile
+        if (Input.GetMouseButton(0) && !firing) { StartCoroutine("FireCoroutine"); }
+
+        //Get scroll wheel input and alter weapon state accordingly
+        scrollInput = Mathf.Clamp((int)Input.mouseScrollDelta.y, -1, 1);
+
+        //Update weapon state using scroll input
+        if (weaponSwapTimer <= 0 && scrollInput != 0 && !firing)
+        {
+            weaponState += scrollInput;
+            weaponSwapTimer = weaponSwapDelay;
+        }
+    }
+
+    void UpdateWeapon()
+    {
+        //Update turret colour
+        turretMaterial.material.color = colourList[(int)weaponState];
+
+        //Update turret projectile
+        currentProjectile = projectiles[(int)weaponState];
+
+        //Update firerate
+        fireRate = currentProjectile.fireRate;
+    }
+
+    void UpdateState()
+    {
+        if (weaponState == WeaponState.firstEnum) { weaponState = WeaponState.lastEnum - 1; }
+        else if (weaponState == WeaponState.lastEnum) { weaponState = WeaponState.firstEnum + 1; }
+
+        //Switch between weapon states
+        switch (weaponState)
+        {
+            case WeaponState.normalProjectile:
+                break;
+            case WeaponState.laserBeam:
+                break;
+        }
+    }
+
+    void FireProjectile()
+    {
+        //Spawn projectile, set position and rotation
+        GameObject newProjectile = Instantiate(currentProjectile, firePoint.position, transform.rotation) as GameObject;
+    }
+
+    void RotateTurret()
+    {
+        //Calculate turret angle using mouse position
+        Vector3 currentPosition = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mouseDirection = Input.mousePosition - currentPosition;
+        float angle = Mathf.Atan2(mouseDirection.x, mouseDirection.y) * Mathf.Rad2Deg;
+        
+        //Rotate turret towards mouse position
+        //Debug.Log("Angle: " + angle);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.up), Time.deltaTime * rotateSpeed);
+        
+        //Debug.DrawRay(firePoint.transform.position, firePoint.transform.forward);
+    }
+
+    IEnumerator FireCoroutine()
+    {
+        firing = true;
+
+        //Fire Projectile
+        FireProjectile();
+
+        //Get the time that the projectile was fired at
+        timeOfFire = Time.time;
+
+        while (timeOfFire + fireAnimationTime > Time.time)
+        {
+            //Calulate the position that the barrel will translate using animation curves and clamp the value between 0 and 1
+            backFireTime = inBarrelRecoilCurve.Evaluate((Time.time - timeOfFire) / fireAnimationTime);
+            backFireTime = Mathf.Clamp(backFireTime, minBackFire, maxBackFire);
+
+            //Lerp colours
+            muzzelMaterial.material.color = Color.Lerp(Color.gray, colourList[(int)weaponState], (Time.time - timeOfFire) / fireAnimationTime);
+            muzzel.transform.localScale = Vector3.Lerp(initialMuzzelSize, initialMuzzelSize * muzzelExpansionSize, (Time.time - timeOfFire) / fireAnimationTime);
+
+            //Translate the position of the barrel using animation curves
+            barrel.transform.localPosition = new Vector3(barrelStartPosition.x, barrelStartPosition.y, barrelStartPosition.z + backFireTime);
+
+            yield return null;
+        }
+
+        while (timeOfFire + fireRate > Time.time)
+        {
+            //Calulate the position that the barrel will translate using animation curves and clamp the value between 0 and 1
+            backFireTime = outBarrelRecoilCurve.Evaluate((Time.time - timeOfFire) / fireRate - fireAnimationTime);
+            backFireTime = Mathf.Clamp(backFireTime, minBackFire, maxBackFire);
+
+            //Lerp colours
+            muzzelMaterial.material.color = Color.Lerp(colourList[(int)weaponState], Color.gray, (Time.time - timeOfFire) / fireRate);
+            muzzel.transform.localScale = Vector3.Lerp(initialMuzzelSize * muzzelExpansionSize, initialMuzzelSize, (Time.time - timeOfFire) / fireRate);
+
+            //Translate the position of the barrel using animation curves
+            barrel.transform.localPosition = new Vector3(barrelStartPosition.x, barrelStartPosition.y, barrelStartPosition.z + backFireTime);
+
+            yield return null;
+        }
+
+        firing = false;
+    }
+    #region Ignore
+    void FireProjectile(Vector3[] directions, int ammo)
+    {
+
+        List<Action> MyActions = new List<Action>();
+
+        MyActions.Add(FireProjectile);
+        MyActions.Add(FireProjectile);
+
+        //Spawn projectile, set position and rotation
+        GameObject newProjectile = Instantiate(currentProjectile, firePoint.position, transform.rotation) as GameObject;
+
+        timeOfFire = Time.time;
+
+        //StartCoroutine("FireCoroutine");
+    }
+
+    void FireProjectile(int Ammo)
+    {
+        List<Action> MyActions = new List<Action>();
+
+        MyActions.Add(FireProjectile);
+        MyActions.Add(FireProjectile);
+
+        //Spawn projectile, set position and rotation
+        GameObject newProjectile = Instantiate(currentProjectile, firePoint.position, transform.rotation) as GameObject;
+        //newProjectile.GetComponent<>()
+        timeOfFire = Time.time;
+        //StartCoroutine("FireCoroutine");
+    }
+    #endregion
+}
